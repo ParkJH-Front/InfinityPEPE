dc29ce142b35af0dfad9307fb866ade8fc25609b
vasdnk <jae1590@gmail.com>
2022-07-27 22:53:36 +0900

    07.27
    아니 왜 쿼리를 못받는데 씨12발

--- infinitypepe/src/App.js
+++ infinitypepe/src/App.js
@@ -31,24 +31,34 @@ function Content() {
 
 function PepeRander() {
   // const API = "https://dapi.kakao.com/v2/search/image";
-  const [searchData, setSearchData] = useState("");
+  const [searchData, setSearchData] = useState([]);
+  const [btn, setBtn] = useState("");
+  let counter = 1;
+  const query = "pepe";
+  const onClick = () => setBtn((counter = counter + 1));
 
   useEffect(() => {
-    fetch("https://dapi.kakao.com/v2/search/image", {
+    fetch("https://dapi.kakao.com/v2/search/web", {
       headers: {
-        Authorization: "17d6f89d24fa2565f0e7155dc37188f0",
+        Authorization: "KakaoAK 17d6f89d24fa2565f0e7155dc37188f0",
       },
-      data: {
-        query: "pepe",
-      },
+      query: String(query),
+      // sort: "accuracy", accuracy(정확도순) 또는 recency(최신순)
+      // page: 1, 결과 페이지 번호, 1~50 사이의 값, 기본 값 1
+      // size: 5, 한 페이지에 보여질 문서 수, 1~80 사이의 값, 기본 값 80
     })
       .then((res) => res.json())
-      .then((data) => setSearchData(data));
-  }, []);
+      .then((data) => {
+        console.log(String(query));
+        setSearchData(data);
+      });
+  }, [btn]);
+
+  console.log(searchData);
 
   return (
     <div>
-      <p>{searchData}</p>
+      <button onClick={onClick}>fucking KAKAO</button>
     </div>
   );
 }
