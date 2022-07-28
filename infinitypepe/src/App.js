16b1dc0713399e0bd66b92f5463c86ccea107c4c
ParkJH-Front <100084383+ParkJH-Front@users.noreply.github.com>
2022-07-28 17:12:58 +0900

    07.28
    API 연동완료
    데이터 가지고 놀아보자
    props의 공부가 필요하다

--- infinitypepe/src/App.js
+++ infinitypepe/src/App.js
@@ -11,54 +11,59 @@ function Nav() {
   );
 }
 
-function Content() {
-  return (
-    <div>
-      <div className="contants">
-        <p>컨텐츠박스</p>
-      </div>
-    </div>
-  );
-}
-
-// url = "https://dapi.kakao.com/v2/search/image"
-// headers = {
-//     "Authorization" : "KakaoAK <REST_API 앱키를 입력하세요>"
-// }
-// data = {
-//   "query" : "펭수"
+// function Box(props) {
+//   const [imgArr, setImgArr] = useState(props);
+//   console.log("왜?");
+//   console.log(imgArr);
+//   return (
+//     <div>
+//       <h1>test</h1>
+//     </div>
+//   );
 // }
 
-function PepeRander() {
-  // const API = "https://dapi.kakao.com/v2/search/image";
-  const [searchData, setSearchData] = useState([]);
-  const [btn, setBtn] = useState("");
-  let counter = 1;
-  const query = "pepe";
-  const onClick = () => setBtn((counter = counter + 1));
+function Main() {
+  const [word, setWord] = useState("");
+  const [trigger, setTrigger] = useState(false);
+  const [searchData, setSearchData] = useState([]); //searchData 에 결과가 들어있음.. 어케뺌?
+
+  const onChange = (event) => setWord(event.target.value);
+
+  const onSubmit = (event) => {
+    event.preventDefault();
+    if (word === "") {
+      return;
+    }
+
+    if (trigger === false) {
+      setTrigger(true);
+    } else {
+      setTrigger(false);
+    }
+  };
 
   useEffect(() => {
-    fetch("https://dapi.kakao.com/v2/search/web", {
+    fetch(`https://dapi.kakao.com/v2/search/image?query=${word}`, {
       headers: {
         Authorization: "KakaoAK 17d6f89d24fa2565f0e7155dc37188f0",
       },
-      query: String(query),
-      // sort: "accuracy", accuracy(정확도순) 또는 recency(최신순)
-      // page: 1, 결과 페이지 번호, 1~50 사이의 값, 기본 값 1
-      // size: 5, 한 페이지에 보여질 문서 수, 1~80 사이의 값, 기본 값 80
     })
-      .then((res) => res.json())
+      .then((req) => req.json())
       .then((data) => {
-        console.log(String(query));
         setSearchData(data);
+        setWord("");
       });
-  }, [btn]);
-
-  console.log(searchData);
-
+  }, [trigger]);
   return (
     <div>
-      <button onClick={onClick}>fucking KAKAO</button>
+      <form onSubmit={onSubmit}>
+        <input
+          onChange={onChange}
+          value={word}
+          type="text"
+          placeholder="findIMG"
+        />
+      </form>
     </div>
   );
 }
@@ -67,8 +72,7 @@ function App() {
   return (
     <div className="container">
       <Nav />
-      <Content />
-      <PepeRander />
+      <Main />
     </div>
   );
 }
