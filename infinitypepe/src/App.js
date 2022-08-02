8757505afe9bfd665df8da54000557e99034a8fd
vasdnk <jae1590@gmail.com>
2022-08-02 23:35:40 +0900

    08.02
    onerror 의 속성을 공부햇당

--- infinitypepe/src/App.js
+++ infinitypepe/src/App.js
@@ -1,14 +1,6 @@
 import { useEffect, useState } from "react";
 import ImgBox from "./ImgBox";
 
-// 이미지 검색한 결과를 출력하는 순서
-// 1. 위치는 최상위 (props 활용을 위해)
-// 2. input 을 통해 사용자로부터 입력값을 받음 (value)
-// 3. onsubmit 이벤트 발생 시 API를 불러옴
-// 4. API URL 내 value를 통해 검색을 진행
-// 5. 검색한 결과를 리턴하여 h1 테그에 전달
-// 6. input 안에 있는 내용을 지워버림.
-
 function App() {
   const [word, setWord] = useState("");
   const [search, setSearch] = useState("");
@@ -36,14 +28,7 @@ function App() {
         Authorization: "KakaoAK 17d6f89d24fa2565f0e7155dc37188f0",
       },
     })
-      .then(
-        (req) =>
-          console.log(req)
-          req.json()
-        // if ((req.ok = true)) {
-        //   return req.json();
-        // }
-      )
+      .then((req) => req.json())
       .then((data) => {
         // OBJ => ARR 변경(Objdct.values()) 데이터 다루는 아주 중요한 구문
         setRowData(
--- infinitypepe/src/ImgBox.js
+++ infinitypepe/src/ImgBox.js
@@ -1,13 +1,28 @@
 import style from "./imgBox.module.css";
+import errorPepe from "./pepe.jpg";
 
 function ImgBox(props) {
+  const onErrorImg = (err) => {
+    // 이미지를 날림
+    err.target.className = "style.none";
+    // 이미지를 페페로 바꿈
+    // err.target.src = errorPepe;
+  };
+
   return (
     <div className={style.container}>
       {props?.imgURLArr.map((img, index) => (
         // 키값 수정할것과, 디자인, 에러의 대한 처리 필요
-        <img className={style.box} key={index} src={img}></img>
+        <img
+          onError={onErrorImg}
+          alt=""
+          className={style.box}
+          key={index}
+          src={img}
+        />
       ))}
     </div>
   );
 }
+
 export default ImgBox;
