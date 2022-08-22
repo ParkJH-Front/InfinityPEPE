de30840caf6c701d993311c4ad69f624082f75d4
ParkJH-Front <100084383+ParkJH-Front@users.noreply.github.com>
2022-08-22 17:37:31 +0900

    08.22
    회원가입 로직 완성
    게시판 작업 시작

--- infinitypepe/src/component/Login.js
+++ infinitypepe/src/component/Login.js
@@ -1,4 +1,5 @@
 import { useEffect, useRef, useState } from "react";
+import { useNavigate } from "react-router-dom";
 import style from "../css/Login.module.css";
 
 function Login() {
@@ -7,6 +8,12 @@ function Login() {
   // useRef = DOM 객체 or element 에 접근하는 HOOK
   const inputRef = useRef();
   // 최초 한번 렌더링 시 포커스 위치를 지정.
+  const loginBoxRef = useRef();
+  const signUpBoxRef = useRef();
+
+  const navigate = useNavigate();
+  const moveSignup = (event) => navigate(`/signup`);
+
   useEffect(() => {
     inputRef.current.focus();
   }, []);
@@ -32,14 +39,13 @@ function Login() {
       .then((json) => {
         const userID = json.id;
         const userPW = json.password;
-        console.log(userID, userPW);
         if (userID === undefined) {
           alert("ID가 존재하지 않습니다.");
         } else if (userPW !== pw) {
           alert("PW가 존재하지 않습니다.");
         } else {
-          console.log(userID, userPW);
-          console.log("떳냐?");
+          loginBoxRef.current.className = "none";
+          signUpBoxRef.current.className = "show";
         }
       });
   };
@@ -47,32 +53,40 @@ function Login() {
   return (
     <div>
       <div className={style.row}>
-        <button className={style.btn}>회원가입</button>
-
-        <form className={style.loginform} onSubmit={onSubmit}>
-          <span className={style.text}>ID :</span>
-          <input
-            className={style.input}
-            ref={inputRef}
-            onKeyDown={onkeyDown}
-            onChange={onChangeID}
-            placeholder="writh your ID"
-            type="text"
-            required
-          ></input>
-          <span className={style.text}>PW :</span>
-          <input
-            className={style.input}
-            ref={inputRef}
-            onChange={onChangePW}
-            placeholder="writh your PW"
-            type="password"
-            required
-          ></input>
-          <button className={style.btn} type="submit">
-            Enter
-          </button>
-        </form>
+        <div ref={loginBoxRef}>
+          <div>
+            <form className={style.loginform} onSubmit={onSubmit}>
+              <span className={style.text}>ID :</span>
+              <input
+                className={style.input}
+                ref={inputRef}
+                onKeyDown={onkeyDown}
+                onChange={onChangeID}
+                placeholder="writh your ID"
+                type="text"
+                required
+              ></input>
+              <span className={style.text}>PW :</span>
+              <input
+                className={style.input}
+                ref={inputRef}
+                onChange={onChangePW}
+                placeholder="writh your PW"
+                type="password"
+                required
+              ></input>
+              <button className={style.btn} type="submit">
+                Enter
+              </button>
+            </form>
+            <button onClick={moveSignup} className={style.btn}>
+              회원가입
+            </button>
+          </div>
+        </div>
+      </div>
+      <div className="none" ref={signUpBoxRef}>
+        <h1>환영합니다 ${id}님.</h1>
       </div>
     </div>
   );
--- infinitypepe/src/component/SignUp.js
+++ infinitypepe/src/component/SignUp.js
@@ -3,14 +3,42 @@ import { useState } from "react";
 function SignUp() {
   const [ID, setID] = useState("");
   const [PW, setPW] = useState("");
+  const URL = "http://localhost:4000/Profiles";
 
   const IDvalue = (event) => setID(event.target.value);
   const PWvalue = (event) => setPW(event.target.value);
 
   const onSubmit = (event) => {
     event.preventDefault();
-    console.log(event);
+    APICheck(ID, PW);
   };
+
+  function APICheck(ChkID, ChkPW) {
+    fetch(`${URL}/${ChkID}`).then((req) => {
+      if (req.status === 200) {
+        alert("이미 존재하는 계정입니다.");
+        return;
+      } else {
+        APICreate(ChkID, ChkPW);
+      }
+    });
+  }
+
+  function APICreate(NewID, NewPW) {
+    fetch(URL, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        id: NewID,
+        password: NewPW,
+        email: `${NewID}@ezimg.com`,
+        name: `귀요미${NewID}`,
+      }),
+    });
+  }
+
   return (
     <div>
       <div>
