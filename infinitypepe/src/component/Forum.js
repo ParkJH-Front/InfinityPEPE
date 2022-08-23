ae98248d8ae15fd915c83abceffa336d78121ffd
ParkJH-Front <100084383+ParkJH-Front@users.noreply.github.com>
2022-08-23 17:16:20 +0900

    08.23
    cors 문제 해결 필요 (게시글 삭제 기능)

--- infinitypepe/src/component/Forum.js
+++ infinitypepe/src/component/Forum.js
@@ -1,7 +1,52 @@
+import { useEffect, useState } from "react";
+import Login from "./Login";
+
 function Forum() {
+  const URL = "http://localhost:4000/Forum";
+
+  const [APIdata, setAPIdata] = useState([]);
+
+  function createAPI() {}
+
+  useEffect(() => {
+    fetch(URL)
+      .then((req) => req.json())
+      .then((json) => {
+        const data = json;
+        setAPIdata(data);
+      });
+  }, []);
+
+  const delClick = (event) => {
+    delectAPI();
+  };
+
+  function delectAPI() {
+    fetch("http://localhost:4000/Forum/두번째 글", {
+      method: "DELECT",
+    }).then((req) => req.json());
+  }
+
   return (
     <div>
-      <h1>포러엄</h1>
+      <Login />
+      <div>
+        <button onClick={createAPI}>글작성</button>
+        <div></div>
+        <div>
+          {APIdata.map((array, index) => (
+            <div key={index}>
+              <h1>{array.title}</h1>
+              <span>{array.date}</span>
+              <span>{array.id}</span>
+              <span>{array.count}</span>
+              <p>{array.body}</p>
+              <p>{array.comment}</p>
+              <button onClick={delClick}>글삭제</button>
+            </div>
+          ))}
+        </div>
+      </div>
     </div>
   );
 }
